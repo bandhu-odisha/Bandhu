14d24546243878c91591017fd8bc5e595093fb1d
Aman Chande <40210413+amanchande@users.noreply.github.com>
2020-05-25 11:35:15 +0530

    updated-models

--- bandhuapp/admin.py
+++ bandhuapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Charity, Activity, Meeting, Ashram
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Charity)
+admin.site.register(Activity)
+admin.site.register(Meeting)
+admin.site.register(Ashram)
