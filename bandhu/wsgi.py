a8097a3c5ce27f9d68131763f2217dad48e240c0
dynamoh <guptaheet53@gmail.com>
2020-06-08 00:15:47 +0530

    Landing Page

--- bandhu/wsgi.py
+++ bandhu/wsgi.py
@@ -8,6 +8,16 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# OR, the same with increased verbosity
+load_dotenv(verbose=True)
+
+# OR, explicitly providing path to '.env'
+from pathlib import Path  # python3 only
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 from django.core.wsgi import get_wsgi_application
 
