98d9c1c98933c75e341b077b83e95686f0dd4e60
amanchande <2018312@iiitdmj.ac.in>
2020-06-09 20:51:14 +0530

    secret_key

--- bandhu/settings.py
+++ bandhu/settings.py
@@ -11,7 +11,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY_CODE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
