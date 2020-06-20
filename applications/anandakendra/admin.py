aa1ae7d47ce197f0ebd78a97f24ff54480241676
dynamoh <guptaheet53@gmail.com>
2020-06-20 15:11:26 +0530

    Admin Configured

--- applications/anandakendra/admin.py
+++ applications/anandakendra/admin.py
@@ -5,10 +5,41 @@ from .models import (
     Student, Acharya, Photo,
 )
 
-# Register your models here.
-admin.site.register(AnandaKendra)
-admin.site.register(ActivityCategory)
-admin.site.register(Activity)
-admin.site.register(Student)
-admin.site.register(Acharya)
-admin.site.register(Photo)
+@admin.register(AnandaKendra)
+class AnandaKendraAdmin(admin.ModelAdmin):
+    list_display = ('name','locality', 'admin')
+    ordering = ('name',)
+    search_fields = ('name', 'admin__first_name','locality')
+
+@admin.register(ActivityCategory)
+class ActivityCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','category',)
+    list_filter = ('category',)
+    ordering = ('name',)
+    search_fields = ('name', 'category')
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('name','kendra','category', 'activity_date')
+    ordering = ('-activity_date',)
+    search_fields = ('name', 'kendra__name','category__name')
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name','kendra','guardian_name', 'contact_no','school_class')
+    ordering = ('name',)
+    list_filter = ('school_class',)
+    search_fields = ('name', 'kendra__name','guardian_name')
+
+@admin.register(Acharya)
+class AcharyaAdmin(admin.ModelAdmin):
+    list_display = ('acharya_id','kendra',)
+    ordering = ('acharya_id',)
+    search_fields = ('acharya_id', 'kendra__name',)
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('kendra','activity','approved')
+    ordering = ('kendra',)
+    list_filter = ('approved',)
+    search_fields = ('activity', 'kendra__name',)
--- applications/ankurayan/admin.py
+++ applications/ankurayan/admin.py
@@ -5,10 +5,41 @@ from .models import (
     ActivityCategory, Activity, Photo,
 )
 
-# Register your models here.
-admin.site.register(Ankurayan)
-admin.site.register(Participant)
-admin.site.register(Guest)
-admin.site.register(ActivityCategory)
-admin.site.register(Activity)
-admin.site.register(Photo)
+@admin.register(Ankurayan)
+class AnkurayanAdmin(admin.ModelAdmin):
+    list_display = ('theme','year','start_date','end_date', 'admin')
+    ordering = ('year',)
+    search_fields = ('year','theme','admin__first_name','locality')
+
+@admin.register(ActivityCategory)
+class ActivityCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','category',)
+    list_filter = ('category',)
+    ordering = ('name',)
+    search_fields = ('name', 'category')
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('name','ankurayan','category', 'activity_date','winner','runner_up1','runner_up1')
+    ordering = ('-activity_date',)
+    search_fields = ('name', 'ankurayan__year','ankurayan__theme','category__name')
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('name','ankurayan', 'contact_no','school_class')
+    ordering = ('name',)
+    list_filter = ('school_class',)
+    search_fields = ('name', 'ankurayan__year','ankurayan__theme',)
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('name','ankurayan','profession','email','contact_no')
+    ordering = ('name',)
+    search_fields = ('name','ankurayan__year','ankurayan__theme',)
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('ankurayan','activity','approved')
+    ordering = ('ankurayan',)
+    list_filter = ('approved',)
+    search_fields = ('activity', 'ankurayan__year','ankurayan__theme',)
