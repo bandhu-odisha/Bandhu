92ffca8d6c962365d8590a9dc9e614ccf17387a4
Priyansh Garg <priyanshgarg30@gmail.com>
2020-06-28 19:45:07 +0530

    events in ashram

--- applications/ashram/admin.py
+++ applications/ashram/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 
 from .models import (
     Ashram, ActivityCategory, Activity,
-    Meeting, Attendee, Photo,
+    Event, Meeting, Attendee, Photo,
 )
 
 # Register your models here.
 admin.site.register(Ashram)
 admin.site.register(ActivityCategory)
 admin.site.register(Activity)
+admin.site.register(Event)
 admin.site.register(Meeting)
 admin.site.register(Attendee)
 admin.site.register(Photo)
