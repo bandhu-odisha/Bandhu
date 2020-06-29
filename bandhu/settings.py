593def93c0b6f209b58a704114f467ce9139b278
Priyansh Garg <priyanshgarg30@gmail.com>
2020-06-29 22:22:35 +0530

    production settings

--- bandhu/settings.py
+++ bandhu/settings.py
@@ -5,6 +5,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -13,11 +14,11 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # TEMPLATE_DEBUG = True
 # DEBUG_PROPAGATE_EXCEPTIONS = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ast.literal_eval(config("ALLOWED_HOSTS"))
 
 
 # Application definition
@@ -81,10 +82,23 @@ WSGI_APPLICATION = 'bandhu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': config("DB_NAME"),
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASS"),
+        'HOST' : config("DB_HOST"),
+        'OPTIONS': {
+          'autocommit': True,
+        },
     }
 }
 
@@ -138,16 +152,16 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 # EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
-EMAIL_USE_TLS = config("EMAIL_USE_TLS")
+# EMAIL_USE_TLS = (config("EMAIL_USE_TLS") == 'True')
+EMAIL_USE_SSL = (config("EMAIL_USE_SSL") == 'True')
 EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_PORT = int(config("EMAIL_PORT"))
 
 # EMAIL ADDRESSES
 SENDER_EMAIL = config("SENDER_EMAIL")
 ADMINS_EMAIL = ast.literal_eval(config("ADMINS_EMAIL"))
-# ADMINS_EMAIL = config("ADMINS_EMAIL")
 
 # Social AUTH Keys
 SOCIAL_AUTH_FACEBOOK_KEY = config("SOCIAL_AUTH_FACEBOOK_KEY")
@@ -162,16 +176,12 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET")
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = config("STATIC_ROOT")
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = config("MEDIA_ROOT")
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_DIRS = [ BASE_DIR+"/static", ]
