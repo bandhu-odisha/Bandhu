33449ca93a0920015e06ff0305d4f61b16877de9
Priyansh Garg <priyanshgarg30@gmail.com>
2020-07-03 02:21:31 +0530

    publications file fix

--- applications/publications/views.py
+++ applications/publications/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
+from django.contrib.sites.shortcuts import get_current_site
 
 from .models import Publication
 
@@ -10,5 +10,11 @@ def index(request):
 
 def publication_detail(request, slug):
     publication = get_object_or_404(Publication, slug=slug, is_visible=True)
+    current_site = get_current_site(request)
 
-    return render(request, "publication_detail.html", { 'publication': publication })
+    context = {
+        'publication': publication,
+        'domain': current_site.domain,
+    }
+
+    return render(request, "publication_detail.html", context)
