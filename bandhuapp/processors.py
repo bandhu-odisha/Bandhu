857f1ad8efe29477800a98ed87402b2ced7e68ee
amanchande <2018312@iiitdmj.ac.in>
2020-07-04 08:57:38 +0530

    recent_section

--- bandhuapp/processors.py
+++ bandhuapp/processors.py
@@ -1,10 +1,10 @@
 from .models import Profile
 from applications.anandakendra.models import Acharya
-from applications.anandakendra.models import Activity as anandakendra_activity
+from applications.anandakendra.models import Event as anandakendra_activity
 from applications.ankurayan.models import Activity as ankurayan_activity
-# from applications.sanskarbarga.models import Activity as sanskarbarga_activity
 from applications.charitywork.models import Activity as charitywork_activity
-from applications.ashram.models import Activity as ashram_activity
+from applications.ashram.models import Event as ashram_activity
+# from applications.sanskarbarga.models import Activity as sanskarbarga_activity
 # from applications.madhmukti.models import Activity as madhmukti_activity
 from bandhuapp.models import RecentActivity as recent_acts
 
@@ -23,15 +23,23 @@ def userList(request):
     
 def recent_activities(request):
     recent_act = recent_acts.objects.all().order_by('-date_created')
-    # anandakendra_act = anandakendra_activity.objects.all().order_by('activity_date')[0]
-    # ankurayan_act = ankurayan_activity.objects.all().order_by('-activity_date').first()
-    # charitywork_act = charitywork_activity.objects.all().order_by('activity_date')[0]
+    return {'recent_activities' :  recent_act}
+
+def recent_section(request):
+    anandakendraAct = anandakendra_activity.objects.all().order_by('-date').first()
+    anandakendra_act = {'act' : anandakendraAct, 'date' : anandakendraAct.date}
+    
+    ankurayanAct = ankurayan_activity.objects.all().order_by('-activity_date').first()
+    ankurayan_act = {'act' : ankurayanAct, 'date' : ankurayanAct.activity_date}
+    
+    charityworkAct = charitywork_activity.objects.all().order_by('-activity_date').first()
+    charitywork_act = {'act' : charityworkAct, 'date' : charityworkAct.activity_date}
+    
+    ashramAct = ashram_activity.objects.all().order_by('-date').first()
+    ashram_act = {'act' : ashramAct, 'date' : ashramAct.date}
     # sanskarbarga_act = sanskarbarga_activity.objects.all().order_by('activity_date')
     # madhmukti_act = madhmukti_activity.objects.all().order_by('activity_date')
-    # ashram_act = ashram_activity.objects.all().order_by('activity_date')[0]
-    # recent_act = [anandakendra_act, ankurayan_act, charitywork_act, ashram_act]
-    # recent_act = []
-    # if ankurayan_act:
-    #     recent_act = [ankurayan_act]
-    # recent_act.sort(key=lambda x: x.activity_date, reverse=True)
-    return {'recent_activities' :  recent_act}
+
+    recent_sec = [anandakendra_act, ankurayan_act, charitywork_act, ashram_act]
+    # recent_sec.sort(key=lambda x: x.date, reverse=True)
+    return {'recent_sec' : recent_sec}
