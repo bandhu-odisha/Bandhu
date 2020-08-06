5fa35681a29a4f1bea8bc6ac602eb630ba4f3301
Priyansh Garg <priyanshgarg30@gmail.com>
2020-08-06 19:06:56 +0530

    Changed header transparency

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,15 @@
 // Collapse Navbar
 // Add styling fallback for when a transparent background .navbar is scrolled
 var navbarCollapse = function() {
-    if($(".navbar.bg-transparent.fixed-top").length === 0) {
-        return;
-    }
-    if ($(".navbar.bg-transparent.fixed-top").offset().top > 50) {
-        $(".navbar").addClass("navbar-scrolled");
-    }
-    else {
-        $(".navbar").removeClass("navbar-scrolled");
-    }
+    // if($(".navbar.bg-transparent.fixed-top").length === 0) {
+    //     return;
+    // }
+    // if ($(".navbar.bg-transparent.fixed-top").offset().top > 50) {
+    //     $(".navbar").addClass("navbar-scrolled");
+    // }
+    // else {
+    //     $(".navbar").removeClass("navbar-scrolled");
+    // }
     if ($(".navbar.bg-transparent.fixed-top").offset().top > 300) {
         $(".navbar").addClass("navbar-scrolled-again");
     }
@@ -18,7 +18,7 @@ var navbarCollapse = function() {
     }
 };
 // Collapse now if page is not at top
-navbarCollapse();
+// navbarCollapse();
 // Collapse the navbar when page is scrolled
 $(window).scroll(navbarCollapse);
 
