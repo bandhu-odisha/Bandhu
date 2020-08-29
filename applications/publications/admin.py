d66ee376118eb0eaa14b69d42af7a4c026dd5547
Priyansh Garg <priyanshgarg30@gmail.com>
2020-08-29 23:55:53 +0530

    Publications banner linked to database

--- applications/publications/admin.py
+++ applications/publications/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Publication
+from .models import Publication, HomePage
 
 @admin.register(Publication)
 class PublicationModelAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title_stripped', 'thumb', 'created')
     list_filter = ('created',)
+
+admin.site.register(HomePage)
--- applications/publications/models.py
+++ applications/publications/models.py
@@ -5,7 +5,7 @@ from bandhuapp.models import Profile
 class Publication(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
-    thumb = models.ImageField(upload_to='publications')
+    thumb = models.ImageField(upload_to='publications/thumb')
     by = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     is_visible = models.BooleanField(default=True)
     created = models.DateTimeField()
@@ -20,3 +20,13 @@ class Publication(models.Model):
     def title_stripped(self):
        from django.utils.html import strip_tags
        return strip_tags(self.title)
+
+class HomePage(models.Model):
+    banner_image = models.ImageField(upload_to='publications/banner')
+
+    class Meta:
+        verbose_name = 'Publications Home Page'
+        verbose_name_plural = 'Publications Home Page'
+
+    def __str__(self):
+        return 'Publications Home Page Content'
